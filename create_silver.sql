cab44acd8294832cd040f84ba253fda54774ebe2
bechara <becharaeliasandre@gmail.com>
2025-12-02 07:52:04 -0300

    added create silver

---
+++ create_silver.sql
@@ -0,0 +1,10 @@
+CREATE TABLE nbalytics_silver.cdnnba_2025 LIKE nbalytics_bronze.cdnnba_2025;
+
+CREATE TABLE nbalytics_silver.matchups_2025 LIKE nbalytics_bronze.matchups_2025;
+
+CREATE TABLE nbalytics_silver.nbastatsv3_2025 LIKE nbalytics_bronze.nbastatsv3_2025;
+
+CREATE TABLE nbalytics_silver.shotdetail_2025 LIKE nbalytics_bronze.shotdetail_2025;
+
+
+
